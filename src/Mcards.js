605eedc7273c7cd537c084c7b9731ed5eacd6bb3
Mohit Khairnar <79041510+mohittk@users.noreply.github.com>
2021-08-31 19:08:54 +0530

    Update Mcards.js

--- src/Mcards.js
+++ src/Mcards.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './vanilla-tilt'
 
 function Card(props) {
   console.log(props);
   return (
     <>
       <div className="cards">
-        <div className="card">
+        <div className="card" data-tilt data-tilt-glare data-tilt-max-glare="0.8">
           <img src={props.imgsrc} alt="mypic" className="card__img" />
           <div className="card__info">
             <span className="card__category">{props.title} </span>
@@ -13,12 +14,10 @@ function Card(props) {
             <a href={props.link} target="blank">
               <button> Watch Now </button>
             </a>
-            <a href={props.linkd} target="blank">
-              <button> Download </button>
-              </a>
           </div>
         </div>
       </div>
+      <script src="vanilla-tilt.js"></script>
     </>
   );
 }
